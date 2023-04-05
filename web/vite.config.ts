82cd0f9eaab20f4dd96c1f79c2e90cf3e87961d6
Phumrapee Limpianchop <git@rayriffy.com>
2023-04-06 03:49:31 +0900

    feat: pwa

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -3,12 +3,48 @@ import { defineConfig } from 'vite'
 import { svelte } from '@sveltejs/vite-plugin-svelte'
 import tsPaths from 'vite-tsconfig-paths'
 import { visualizer } from 'rollup-plugin-visualizer'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     svelte(),
     tsPaths(),
+    VitePWA({
+      manifest: {
+        short_name: 'techcal.dev',
+        name: 'techcal.dev',
+        start_url: '/',
+        scope: '/',
+        display: 'standalone',
+        theme_color: '#111111',
+        background_color: '#ffffff',
+        icons: [
+          {
+            src: '/icons/icon-192x192.png',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: '/icons/icon-256x256.png',
+            sizes: '256x256',
+            type: 'image/png',
+          },
+          {
+            src: '/icons/icon-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+          },
+          {
+            src: '/icons/icon-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'any maskable',
+          },
+        ],
+      },
+    }),
+    // @ts-ignore
     ...(process.env.ANALYZE === 'true'
       ? [
           visualizer({
