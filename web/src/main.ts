a27eecad9e20970221e3a47a66d6f9f4ef9d6880
dtinth on MBP M1 <dtinth@spacet.me>
2023-08-10 19:44:18 +0700

    inter

--- web/src/main.ts
+++ web/src/main.ts
@@ -1,6 +1,8 @@
 import App from './app.svelte'
 import '@fontsource/anuphan/400.css'
 import '@fontsource/anuphan/700.css'
+import '@fontsource/inter/400.css'
+import '@fontsource/inter/700.css'
 import './styles/tailwind.css'
 
 const app = new App({
