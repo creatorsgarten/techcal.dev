a5efe14d21a2b826b0625e7275959dd380fc62a4
dtinth on MBP M1 <dtinth@spacet.me>
2023-08-12 01:36:17 +0700

    gray out tentative events

--- web/src/modules/event/index.svelte
+++ web/src/modules/event/index.svelte
@@ -7,11 +7,21 @@
   export let item: GoogleCalendarItem
   export let dayDiff: number
 
+  $: tentative = (summary => {
+    if (summary.includes('tba')) return 'tba'
+    if (summary.includes('tbc')) return 'tbc'
+    if (summary.includes('tbd')) return 'tbd'
+    if (summary.includes('tentative')) return 'tentative'
+    if (summary.includes('[duplicate]')) return 'duplicate'
+  })(item.summary.toLowerCase())
+
   $: captureHighlighted = $captureEvent?.id === item.id
   $: color = captureHighlighted
     ? 'bg-yellow-200 text-yellow-950 border-orange-500'
+    : tentative
+    ? `border-neutral-950 text-neutral-950 dark:text-neutral-50 dark:border-neutral-400 bg-neutral-100 dark:bg-neutral-500/40`
     : `border-blue-950 text-blue-950 dark:text-blue-50 dark:border-blue-400 bg-blue-100 dark:bg-blue-500/40`
-  $: opacity = dayDiff < 0 ? 'opacity-50' : ''
+  $: opacity = dayDiff < 0 ? 'opacity-50' : tentative ? 'opacity-75' : ''
 
   let onOpen = () => {
     activeEvent.set(item)
