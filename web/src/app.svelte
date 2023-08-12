88fa54b9a03ec482e96b52e4b930ac341cf94382
dtinth on MBP M1 <dtinth@spacet.me>
2023-08-12 21:50:47 +0700

    fix page title

--- web/src/app.svelte
+++ web/src/app.svelte
@@ -55,7 +55,7 @@
     <div class="flex justify-between px-2 sm:px-4 py-2 items-center">
       <div>
         <h1 class="text-2xl sm:text-3xl text-sky-500 dark:text-sky-400">
-          {import.meta.env.VITE_SITE_NAME}
+          {'<' + import.meta.env.VITE_SITE_NAME + ' />'}
         </h1>
         <!-- <p class="text-gray-950 pt-1 text-sm sm:text-base">รวม อัพเดท Tech event, Tech Meetup ในไทยไว้ในที่เดียว</p> -->
       </div>
