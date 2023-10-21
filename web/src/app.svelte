a174a4058a79a75aae8a3b258a831d9ec0f3a05d
Narisara Limpisirisant <56298563+NongSnail@users.noreply.github.com>
2023-10-21 14:59:02 +0000

    Update app.svelte

--- web/src/app.svelte
+++ web/src/app.svelte
@@ -54,7 +54,8 @@
   {#if !$captureMode}
     <div class="flex justify-between px-2 sm:px-4 py-2 items-center">
       <div>
-          <img src="Logo_glitch.svg" alt="Logo" width="40%">
+        <p>{import.meta.env.VITE_SITE_NAME}</p> 
+        <img src="{import.meta.env.VITE_SITE_NAME === 'thai-tech-calendar' ? 'Logo_th.svg' : 'Logo_in.svg'}" alt="{import.meta.env.VITE_SITE_NAME}" width="40%">
         <!-- <p class="text-gray-950 pt-1 text-sm sm:text-base">รวม อัพเดท Tech event, Tech Meetup ในไทยไว้ในที่เดียว</p> -->
       </div>
       <div class="flex items-center ml-2">
